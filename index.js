12de1259d56e2f4bc9d42b51fa5db457c6c0289a
Ranjithanbu <ranjithjithg@gmail.com>
2024-03-13 00:18:33 -0700

    update

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const PORT = 4000;
 //allow to access the port
 
 app.use(cors())
-
+app.get('/',(req,res)=>{
+    res.status(200).json({message:'site is working'})
+})
 // navigate to rooRoute
 
 app.use('/api', roomAppRoute)
